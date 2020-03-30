73ef74ebbfcad0c0cb1ff45dcd91b6e10060cd89
yachmennikov <otsmmm2013@gmail.com>
2020-03-30 17:32:26 +0300

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SearchServise } from './servises/search.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'subject-servise';
+
+  constructor(public searchServise: SearchServise) {}
+
 }
